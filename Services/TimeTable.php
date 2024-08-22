ab9b6a942c31bc84188278861cfde66a029b1c3c
Jeppe Krogh <kjej@aarhus.dk>
2024-08-22 16:15:55 +0200

    Added apihandler symlink to install script

--- Services/TimeTable.php
+++ Services/TimeTable.php
@@ -17,6 +17,7 @@ class TimeTable
      */
     private static array $assets = [
         // source => target
+        __DIR__ . '/../assets/timeTableApiHandler.js' => APP_ROOT . '/public/dist/js/plugin-timeTableApiHandler.v%%VERSION%%.js',
         __DIR__ . '/../assets/timeTable.js' => APP_ROOT . '/public/dist/js/plugin-timeTable.v%%VERSION%%.js',
         __DIR__ . '/../assets/timeTable.css' => APP_ROOT . '/public/dist/css/plugin-timeTable.v%%VERSION%%.css',
     ];
